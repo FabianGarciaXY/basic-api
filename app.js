a958dd3f373472d5d15b3b0ec66a8777e86142c9
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-04-22 20:14:04 -0500

    6: Get Method created for an endpoint that returns a list of users

--- app.js
+++ app.js
@@ -2,11 +2,38 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+app.use(express.json()) // Indicación para usar JSON
 
+/*============   Main route   =============*/
 app.get('/', (req, res) => {
     res.send('Hi World!!!');
 });
 
+
+/*=============================================
+=                   HTTP METHODS              =
+    GET
+
+    Usamos el metodo GET para crear un 
+    endpoind que regresa una lista de personas
+
+=============================================*/
+app.get('/v1/explorers', (req, res) => {
+    console.log(`API Explorers GET ALL requests ${new Date()}`);
+
+    const explorer1 = {id: 1, name: 'explorer1'};
+    const explorer2 = {id: 2, name: 'explorer2'};
+    const explorer3 = {id: 3, name: 'explorer3'};
+
+    const listOfExplorers = [explorer1, explorer2, explorer3];
+    // Codigo 200 HTTP OK, y envio de un JSON
+    res.status(200).json(listOfExplorers);
+});
+
+
+
+/*============  Starting server  =============*/
 app.listen(port, () => {
     console.log('Server Started');
+
 });
