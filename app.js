6dc65ea2049f975d69db8328cec0e77f32718467
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-04-22 19:47:43 -0500

    5: started server

---
+++ app.js
@@ -0,0 +1,12 @@
+// Setting up Express
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hi World!!!');
+});
+
+app.listen(port, () => {
+    console.log('Server Started');
+});
